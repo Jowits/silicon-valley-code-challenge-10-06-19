3faef62c7867a55a469f45daec620daaab2483a6
Jowita <jowita.lapies@gmail.com>
2019-06-10 11:08:51 +0100

    practice code challenge

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,64 @@
+#
+# #### Build the following methods on the `Startup` class
+#
+# - `Startup#name`
+#   - returns a **string** that is the startup's name
+# - `Startup#founder`
+#   - returns a **string** that is the founder's name
+#   - Once a startup is created, the founder cannot be changed.
+# - `Startup#domain`
+#   - returns a **string** that is the startup's domain
+#   - Once a startup is created, the domain cannot be changed.
+# - `Startup#pivot`
+#   - given a string of a **domain** and a string of a **name**, change the domain and name of the startup
+# - `Startup.all`
+#   - should return **all** of the startup instances
+# - `Startup.find_by_founder`
+#   - given a string of a **founder's name**, returns the **first startup** whose founder's name matches
+# - `Startup.domains`
+#   - should return an **array** of all of the different startup domains
+#
+# ---
 class Startup
+
+  attr_accessor :name, :domain
+  attr_reader :founder
+
+  @@all = []
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pivot(domain, name)
+    self.domain = domain
+    self.name = name
+  end
+
+  def self.find_by_founder(name)
+    @@all.find do |startup|
+      startup.founder == name
+    end
+  end
+
+  def self.domains
+    @@all.map do |startup|
+      startup.domain
+    end
+  end
+
+  def sing_contract(venture_capitalist, type, investment)
+    FundingRound.new(self, venture_capitalist, type, investment)
+  end
+
+
+
 
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,45 @@
+# #### Build out the following methods on the `VentureCapitalist` class
+#
+# - `VentureCapitalist#name`
+#   - returns a **string** that is the venture capitalist's name
+# - `VentureCapitalist#total_worth`
+#   - returns a **number** that is the total worth of this investor (e.g., think of it as how much money they have)
+# - `VentureCapitalist.all`
+#   - returns an array of all venture capitalists
+# - `VentureCapitalist.tres_commas_club`
+#   - returns an array of all venture capitalists in the Trés Commas club (they have more then 1,000,000,000 `total_worth`)
+
 class VentureCapitalist
+
+  attr_accessor :name, :total_worth
+
+  @@all = []
+
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club
+    somebodies = @@all.select{|capitalist| capitalist.total_worth >= 1000000000}
+    somebodies.each do |somebody|
+      puts somebody.name
+    end
+  end
+
+  def offer_contract(startup, type, investment)
+    FundingRound.new(startup, self, type, investment)
+  end
+
+  def funding_rounds
+    FundingRound.all.select do |funding_round|
+      funding_round.venture_capitalist.name == self.name
+    end
+  end
 
 end
